f30feac06e601d19815a8900aa487f016d81fb70
MohanadOO <moohand220@gmail.com>
2022-03-14 23:12:20 +0300

    show user info as JSON

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-// server.js
-// where your node app starts
-
-// init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+const { lookup } = require('geoip-lite')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.set('view engine', 'ejs')
 
+// your first API endpoint...
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
 
-
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software,
+    location: lookup(ip),
+  })
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.listen(5500 || process.env.PORT)
